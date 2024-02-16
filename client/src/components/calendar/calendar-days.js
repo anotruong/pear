8fc2322e759eafb21e49848f8abe8941ffcaa388
Mel <74751155+anotruong@users.noreply.github.com>
2024-02-15 16:24:38 -0800

    updated design for calendar

--- client/src/components/calendar/calendar-days.js
+++ client/src/components/calendar/calendar-days.js
@@ -3,13 +3,67 @@ import mockData from '../../mock-data.json';
 
 function CalendarDays(props) {
 
-  const pendingInvite = mockData.pendingInvite;
-  // console.log(pendingin)
+  const tempUserId = '001';
+  const confirmedMeals = mockData.confirmedMeals;
+
+  let currentMonth = String(new Date().getMonth() + 1); //add 1 to the result because Jan is '0'.
+  currentMonth = currentMonth.length === 2 ? currentMonth : '0' + currentMonth;
+  // console.log(pendingInvite.forEach(obj => console.log(new Date(obj.date).getMonth() + 1)))
+  const filteredTemp = confirmedMeals[tempUserId]
+    .map(ele => mockData.pendingInvite.filter(obj => obj.id === ele))
+    // .filter(obj => obj.date.slice(6, 7));
+  // filteredTemp.forEach(obj => console.log(obj))
+  // console.log(filteredTemp.filter(obj => obj.date.slice(6, 7)))
+  // filteredTemp.forEach(obj => console.log(new Date(obj[0].date))
+
+  // create a clone to prevent .map method from mutating object pointed by 'filteredTemp'
+  // let transformedTemp = JSON.parse(JSON.stringify(filteredTemp)); 
+  
+  // transformedTemp = transformedTemp.map(obj => {
+  //   const accDate = new Date(obj[0].date);  
+  //   let year = accDate.getFullYear();
+  //   let month = (accDate.getMonth() + 1).toString().padStart(2, '0');
+  //   let day = accDate.getDate().toString().padStart(2, '0');
+  
+  //   let newDisplayDate = year + month + day;
+  //   obj.date = newDisplayDate;
+  //   // console.log(newDisplayDate);
+  //   return obj;
+  // });
+
+  // const sortedTemp = transformedTemp.sort((a, b) => b.date - a.date);
+
+  // console.log(currentMonth)
+
+  // const currentMonthEvents = sortedTemp.filter(obj => obj.date.slice(4, 6) == currentMonth);
+
+  // console.log(currentMonthEvents)
 
   let firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
   let weekdayOfFirstDay = firstDayOfMonth.getDay();
   let currentDays = [];
 
+  // console.log(firstDayOfMonth)
+
+  /* Appointment Function
+    START obj arr
+    ITERATE through arr
+    EVALUATE array with 'current calendar Date'
+      if the array evaluates to true, return true
+      else if the array evaluates to false, return false
+    RETURN BOOLEAN
+  */
+  const hasAppointment = (dateArg) => {
+    let isTrue = filteredTemp.filter(obj => {
+      let setHour = new Date(new Date(obj[0].date).setHours(0, 0, 0));
+      // Coerce to date type twice because '.setHours' method coerces data to number type
+
+      return setHour.toString() === dateArg.toString();
+      // coerce to string type because primitive data types can strictly evaluate to each other while non-primitive data types evaluate by where the pointer is stored.
+    })
+
+    return isTrue.length === 1;
+  };
 
   for (let day = 0; day < 42; day++) {
     if (day === 0 && weekdayOfFirstDay === 0) {
@@ -26,16 +80,13 @@ function CalendarDays(props) {
       month: firstDayOfMonth.getMonth(),
       number: firstDayOfMonth.getDate(),
       selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
-      year: firstDayOfMonth.getFullYear()
-      // appointment: 'evaluate if there is a pending invite that correlates with value of 'date'
+      year: firstDayOfMonth.getFullYear(),
+      appointment: hasAppointment(new Date(firstDayOfMonth))
     }
 
     currentDays.push(calendarDay);
-    console.log(calendarDay.date)
-
   }
 
-  // console.log(calendarDay)
   /* Create a function that would insert a break in the line of divs.
       Options: 
         - Create 6 variables pointed to 6 empty arrays.
@@ -45,9 +96,7 @@ function CalendarDays(props) {
         DECLARE 6 variables 'week#' that points to empty arrays;
         SORT the array into a variable that's length is less then 7.
         END 6 variables
-
   */
-
 
   let week1 = [];
   let week2 = [];
@@ -57,7 +106,8 @@ function CalendarDays(props) {
   let week6 = [];
 
   currentDays.map((day) => {
-    let divDay =  <div className={"calendar-day" + (day.currentMonth ? " current" : "") + (day.selected ? " selected" : "")}
+    console.log(day)
+    let divDay =  <div className={"calendar-day" + (day.currentMonth ? " current" : "") + (day.selected ? " selected" : "") + (day.appointment ? " app" : "")}
           onClick={() => props.changeCurrentDay(day)}>
       <p>{day.number}</p>
       </div>;
--- client/src/components/monthlyNotes.js
+++ client/src/components/monthlyNotes.js
@@ -8,54 +8,21 @@ const MonthlyNotes = () => {
   const tempUserId = "001";
   const acc = (id) => mockData.accounts.filter(obj => obj.id === id);
 
-  // const tempMockData = [
-  //   {
-  //     date: '07/01',
-  //     time: '12pm - 2pm',
-  //     itinerary: ': w/ aubrey'
-  //   },
-  //   {
-  //     date: '07/05',
-  //     time: '4pm-5pm',
-  //     itinerary: 'dinner w/ janet'
-  //   },
-  //   {
-  //     date: '07/08',
-  //     time: '11pm-1pm',
-  //     itinerary: 'lunch w/ shane'
-  //   },
-  //   {
-  //     date: '07/11',
-  //     time: '7pm-9pm',
-  //     itinerary: 'dinner w/ michael'
-  //   },
-  //   {
-  //     date: '07/11',
-  //     time: '8pm-10pm',
-  //     itinerary: 'dinner w/ haruka'
-  //   },
-  //   {
-  //     date: '07/30',
-  //     time: '7am-9pm',
-  //     itinerary: 'breakfast w/ Agnes'
-  //   },
-  // ];
+  const filteredTemp = mockData.confirmedMeals[tempUserId].map(ele => mockData.pendingInvite.filter(obj => obj.id === ele));
 
-  let filteredTemp = mockData.confirmedMeals[tempUserId].map(ele => mockData.pendingInvite.filter(obj => obj.id === ele))
-  // .filter(obj => );
-
-  console.log(filteredTemp)
+  // create a clone to prevent .map method from mutating object pointed by 'filteredTemp'
+  let transformedTemp = JSON.parse(JSON.stringify(filteredTemp)); 
   
-  let transformedTemp = filteredTemp.map(obj => {
+  transformedTemp = transformedTemp.map(obj => {
     const accDate = new Date(obj[0].date);  
     let year = accDate.getFullYear();
     let month = (accDate.getMonth() + 1).toString().padStart(2, '0');
     let day = accDate.getDate().toString().padStart(2, '0');
   
-    console.log(accDate.getMonth().toString().padStart(2, '0'))
+    // console.log(accDate.getMonth().toString().padStart(2, '0'))
     let newDisplayDate = year + month + day;
     obj.date = newDisplayDate;
-    // console.log(newDisplayDate)
+    // console.log(newDisplayDate);
     return obj;
   })
   // console.log(filteredTemp)
@@ -63,22 +30,26 @@ const MonthlyNotes = () => {
 
 
   // // In the context of sorting Date objects, using getTime() ensures that the sorting is based on the underlying timestamp representation of each date. 
-  // let sortedTemp = transformedTemp.sort((a, b) => b.date - a.date);
+  const sortedTemp = transformedTemp.sort((a, b) => a.date - b.date);
 
-  // // console.log(sortedTemp)
+  let currentMonth = String(new Date().getMonth() + 1); //add 1 to the result because Jan is '0'.
+  currentMonth = currentMonth.length === 2 ? currentMonth : '0' + currentMonth;
+  // console.log(currentMonth)
 
-  // let agendaList = sortedTemp.map(obj => {
-  //   const date = displayMonthDay(obj[0].date);
-  //   const meal = categorizeMeal(obj[0].time);
+  const currentMonthEvents = sortedTemp.filter(obj => obj.date.slice(4, 6) == currentMonth);
 
-  //   const firstName = `${acc(obj[0].userId)[0].firstName.slice(0, 1).toUpperCase()}${acc(obj[0].userId)[0].firstName.slice(1)}`
-  //   const lastInitial = acc(obj[0].userId)[0].lastName.slice(0, 1).toUpperCase();
-  //   // console.log(abbrName)
+  let agendaList = currentMonthEvents.map(obj => {
+    const date = displayMonthDay(obj[0].date);
+    const meal = categorizeMeal(obj[0].time);
 
-  //   return (
-  //     <li className='agenda'><b className='date'>{date}</b> {obj[0].time}: {meal} w/ {firstName} {lastInitial}.</li>
-  //   )
-  // });
+    const firstName = `${acc(obj[0].userId)[0].firstName.slice(0, 1).toUpperCase()}${acc(obj[0].userId)[0].firstName.slice(1)}`
+    const lastInitial = acc(obj[0].userId)[0].lastName.slice(0, 1).toUpperCase();
+    // console.log(abbrName)
+
+    return (
+      <li className='agenda'><b className='date'>{date}</b> {obj[0].time}: {meal} w/ {firstName} {lastInitial}.</li>
+    )
+  });
 
 
   return(
@@ -86,7 +57,7 @@ const MonthlyNotes = () => {
       <div className='notes'>
         <h3>This month at a glance:</h3>
         <ul>
-          {/* {agendaList} */}
+          {agendaList}
           {/* will iterate this and stylelize the information */}
         </ul>
       </div>
